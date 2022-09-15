d774dc25061be5fffe9c8e166b2e12395889414b
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-09-15 21:18:43 +0300

    Project completed

--- bot_activate.py
+++ bot_activate.py
@@ -1,4 +1,11 @@
 from bot import activate_bot
 
+
+def set_config():
+    import os
+    os.system("pip install -r .\\requirements.txt")
+
+
 if __name__ == '__main__':
+    set_config()
     activate_bot()
